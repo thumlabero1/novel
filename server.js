1b8b510e28a2873dfbe7d003879ebf316a95d5e0
Phan_Linh <nhomtoanhsg20172018@gmail.com>
2024-03-14 22:40:14 +0700

    change index

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 2701;
 const mongoose = require('mongoose');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
